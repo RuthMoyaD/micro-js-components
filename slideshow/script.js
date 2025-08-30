647360faaf3c35daf52bfcfb29b95151093d57fd
rmoydan@protonmail.com <rmoydan@protonmail.com>
2025-08-30 07:21:38 +0200

    add final

--- slideshow/script.js
+++ slideshow/script.js
@@ -20,3 +20,64 @@ const changeSlides = () => {
     }
   });
 };
+
+const arrowVisibility = () => {
+  const arrows = document.querySelectorAll(".control");
+  Array.from(arrows).forEach(arrow => {
+    if (!playPauseBool) {
+      arrow.classList.add("arrows-visibility");
+    } else {
+      arrow.classList.remove("arrows-visibility");
+    }
+  });
+};
+
+const changePlayPause = () => {
+  const i = document.querySelector(".play-pause i");
+  const cls = i.classList[1];
+  if (cls === "fa-play") {
+    i.classList.remove("fa-play");
+    i.classList.add("fa-pause");
+  } else {
+    i.classList.remove("fa-pause");
+    i.classList.add("fa-play");
+  }
+};
+
+const playPause = () => {
+  if (playPauseBool) {
+    interval = setInterval(() => {
+      current++;
+      changeSlides();
+    }, 3000);
+    playPauseBool = false;
+  } else {
+    clearInterval(interval);
+    playPauseBool = true;
+  }
+  arrowVisibility();
+  changePlayPause();
+};
+
+document.querySelector(".left-arrow").addEventListener("click", () => {
+  if (!playPauseBool) {
+    playPause();
+  }
+  current--;
+  changeSlides();
+});
+
+document.querySelector(".right-arrow").addEventListener("click", () => {
+  if (!playPauseBool) {
+    playPause();
+  }
+  current++;
+  changeSlides();
+});
+
+document.querySelector(".play-pause").addEventListener("click", () => {
+  playPause();
+});
+
+changeSlides();
+playPause();
